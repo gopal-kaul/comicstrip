974e859c87202e9a4c37da8d7394773ccc31ac77
Gopal Kaul <gopalkaul5@gmail.com>
2021-10-14 21:36:30 +0530

    Project completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Card from "./Components/CustomCard";
+import { Container, Row, Col } from "reactstrap";
 function App() {
+  const row1 = [
+    {
+      gameName: "Grand Theft Auto V",
+      gameImg: "deathnote.jpg",
+    },
+    {
+      gameName: "Need For Speed: Most Wanted 2012",
+      gameImg: "deathnote.jpg",
+    },
+    {
+      gameName: "Red Dead Redemption II",
+      gameImg: "deathnote.jpg",
+    },
+    {
+      gameName: "Counter Strike: Global-Offensive",
+      gameImg: "deathnote.jpg",
+    },
+    {
+      gameName: "VALORANT",
+      gameImg: "deathnote.jpg",
+    },
+  ];
+  const row2 = [
+    {
+      gameName: "Player's Unknown Battle Grounds",
+      gameImg: "deathnote.jpg",
+    },
+    {
+      gameName: "HALO",
+      gameImg: "deathnote.jpg",
+    },
+    {
+      gameName: "Fall Guys",
+      gameImg: "deathnote.jpg",
+    },
+    {
+      gameName: "Rocket League",
+      gameImg: "deathnote.jpg",
+    },
+    {
+      gameName: "Genshin Impact",
+      gameImg: "deathnote.jpg",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      className="App"
+      style={{
+        backgroundImage: `url(${process.env.PUBLIC_URL + "assets/back.png"})`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        height: "100vh",
+      }}
+    >
+      <Container style={{ paddingTop: "0.25%" }}>
+        <h1
+          style={{
+            fontFamily: "Patrick Hand",
+            fontWeight: 900,
+            fontSize: "7rem",
+            textAlign: "center",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          COMIC STRIP
+        </h1>
+        <Row className="myRow" style={{ marginBottom: "2.5%" }}>
+          {row1.map((item, index) => {
+            return (
+              <Col key={index}>
+                <Card gameImg={item.gameImg} gameName={item.gameName} />
+              </Col>
+            );
+          })}
+        </Row>
+        <Row className="myRow">
+          {row2.map((item, index) => {
+            return (
+              <Col key={index}>
+                <Card gameImg={item.gameImg} gameName={item.gameName} />
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ src/Components/CustomCard.js
@@ -0,0 +1,37 @@
+import ReactCardFlip from "react-card-flip";
+import { useState } from "react";
+
+import { Card, CardImg, CardBody, CardTitle, Button } from "reactstrap";
+const CustomCard = ({ gameImg, gameName }) => {
+  const [flip, setFlip] = useState(false);
+  const handleClick = (e) => {
+    e.preventDefault();
+    setFlip(true);
+  };
+  return (
+    <div>
+      <ReactCardFlip
+        isFlipped={flip}
+        flipDirection="horizontal"
+        flipSpeedFrontToBack={1}
+      >
+        <>
+          <button
+            style={{ backgroundColor: "transparent", borderStyle: "hidden"}}
+            onClick={handleClick}
+          >
+            <img top width="100%" src="assets/card_back.png" alt={gameName} style={{borderRadius:"4%"}}/>
+          </button>
+        </>
+        <Card style={{width:232, height:348}}>
+          <CardImg top width="100%" src={`assets/${gameImg}`} alt={gameName} />
+          <CardBody>
+            <CardTitle tag="h3" style={{textAlign: "center"}}>{gameName}</CardTitle>
+          </CardBody>
+        </Card>
+      </ReactCardFlip>
+    </div>
+  );
+};
+
+export default CustomCard;
